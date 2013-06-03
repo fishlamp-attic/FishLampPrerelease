1a96467a7560217fb707e1fb6fc43e59870dc9b3
Mike Fullerton <mike@greentongue.com>
2013-06-03 12:55:51 -0700

    move stuff to sensible locations, finally

---
+++ FLCommandLineArgument.h
@@ -0,0 +1,31 @@
+//
+//  FLCommandLineArgument.h
+//  FishLampCommandLineTool
+//
+//  Created by Mike Fullerton on 11/14/12.
+//  Copyright (c) 2013 GreenTongue Software LLC, Mike Fullerton. 
+//  The FishLamp Framework is released under the MIT License: http://fishlamp.com/license 
+//
+
+#import "FLCocoaRequired.h"
+#import "FLCommandLineParser.h"
+
+@interface FLCommandLineArgument : NSObject {
+@private
+    NSString* _key;
+    NSMutableArray* _values;
+}
+
+@property (readonly, strong, nonatomic) NSString* key;
+@property (readonly, strong, nonatomic) NSArray* values;
+
+- (id) initWithKey:(NSString*) key;
++ (id) commandLineArgument:(NSString*) key;
+
+- (void) addValue:(NSString*) param;
+
+- (NSString*) parameterString;
+
+@end
+
+
