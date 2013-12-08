fd798ca6d8b33091d1478795f6a330b7a1adf07e
Mike Fullerton <mike@Mikes-MacPro.local>
2013-12-08 12:28:24 -0800

    gather script

---
+++ FishLampCocoa/gather-source.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+SourceDir="$1"
+FolderName="$2"
+
+pushd "$SourceDir" > /dev/null || { echo "$SourceDir not found"; exit 1; }
+SourceDir=`pwd` 
+popd > /dev/null
+
+ClassFolderName=`basename $SourceDir`
+DestFolder="`pwd`/Classes/$ClassFolderName";
+
+if [ -d "$DestFolder" ]; then
+	rm -rd "$DestFolder"
+fi
+
+mkdir -p "$DestFolder"
+
+echo "Searching in \"$SourceDir\""
+
+exit 0
+
+dirs=`find "$SourceDir" -depth 1 -type d`
+
+for dir in $dirs; do	
+	theFolder=`basename $dir`
+	theFolder="$DestFolder/$theFolder"
+
+	mkdir "$theFolder"
+	
+	toFind="$dir/$FolderName"
+	
+	if [ -d "$toFind" ]; then
+
+		files=`find "$toFind" -name "*.[mh]"`
+
+		for file in $files; do
+			srcFile="`pwd`/$file"
+			destFile=`basename "$file"`
+			destFile="$theFolder/$destFile" 
+# 			echo "old: $srcFile"
+# 			echo "new: $destFile"
+			
+			ln -f "$srcFile" "$destFile"
+		done
+	fi
+done
---
+++ Podfile
@@ -0,0 +1,7 @@
+pod 'ObjectiveSugar', '~> 0.5'
+
+post_install do |installer|
+  installer.project.targets.each do |target|
+    puts "#target.name"
+  end
+end
